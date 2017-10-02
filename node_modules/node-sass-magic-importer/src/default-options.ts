4bcca91d1dc506dd7019eeadc66edff751792acb
Zach Beauvais <z.beauvais@gmail.com>
2017-10-01 21:13:20 -0400

    initial commit

---
+++ node_modules/node-sass-magic-importer/src/default-options.ts
@@ -0,0 +1,30 @@
+import { ICustomFilter } from 'css-node-extract/src/interfaces/ICustomFilter';
+
+export const defaultOptions: {
+  cwd: string;
+  extensions: string[];
+  packageKeys: string[];
+  packagePrefix: string;
+  disableImportOnce: boolean;
+  customFilters?: ICustomFilter[];
+} = {
+  cwd: process.cwd(),
+  extensions: [
+    `.scss`,
+    `.sass`,
+  ],
+  packageKeys: [
+    `sass`,
+    `scss`,
+    `style`,
+    `css`,
+    `main.sass`,
+    `main.scss`,
+    `main.style`,
+    `main.css`,
+    `main`,
+  ],
+  packagePrefix: `~`,
+  disableImportOnce: false,
+  customFilters: undefined,
+};
